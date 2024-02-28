0ff041ba5977de3f3141a80ef1de425613500aac
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-02-28 20:17:25 +0100

    test

--- app/Http/Livewire/Monitores/EditComponent.php
+++ app/Http/Livewire/Monitores/EditComponent.php
@@ -90,7 +90,7 @@ class EditComponent extends Component
     public function filtrarPorFecha()
     {
         // Asegúrate de validar o establecer valores predeterminados para las fechas
-        $this->eventos = Evento::whereBetween('fecha', [$this->fechaInicio, $this->fechaFin])->get();
+        $this->eventos = Evento::whereBetween('diaEvento', [$this->fechaInicio, $this->fechaFin])->get();
         // Aquí deberías ajustar la consulta a tus necesidades, por ejemplo, uniendo con otras tablas o filtrando por ID de monitor
     }
     public function getEventoFromIdServicioEvento($id)
