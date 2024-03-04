1b7c711aa94339d30645516130625148722580ae
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-03-04 23:19:24 +0100

    test

--- resources/views/livewire/disponible/index-component.blade.php
+++ resources/views/livewire/disponible/index-component.blade.php
@@ -76,12 +76,11 @@
         });
     });
 </script>
-{{-- <script>
+<script>
     $(document).ready(function() {
         // Inicializar DataTables
         $('#datatable-buttons').DataTable({
             paging: false, // Deshabilita la paginación
-            pageLength: 1000,
             searching: true, // Habilita la búsqueda (opcional, ajústalo según tus necesidades)
             ordering: true, // Habilita el ordenamiento de columnas (opcional, ajústalo según tus necesidades)
             // Quitar o ajustar según la necesidad de exportar datos (PDF, Excel)
@@ -92,5 +91,25 @@
             responsive: true // Asegura que la tabla sea responsive
         });
     });
-</script> --}}
+    </script>
+<!-- Required datatable js -->
+<script src="../assets/js/jquery.slimscroll.js"></script>
+
+<script src="../plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../plugins/datatables/dataTables.bootstrap4.min.js"></script>
+<!-- Buttons examples -->
+<script src="../plugins/datatables/dataTables.buttons.min.js"></script>
+<script src="../plugins/datatables/buttons.bootstrap4.min.js"></script>
+<script src="../plugins/datatables/jszip.min.js"></script>
+<script src="../plugins/datatables/pdfmake.min.js"></script>
+<script src="../plugins/datatables/vfs_fonts.js"></script>
+<script src="../plugins/datatables/buttons.html5.min.js"></script>
+<script src="../plugins/datatables/buttons.print.min.js"></script>
+<script src="../plugins/datatables/buttons.colVis.min.js"></script>
+<!-- Responsive examples -->
+<script src="../plugins/datatables/dataTables.responsive.min.js"></script>
+<script src="../plugins/datatables/responsive.bootstrap4.min.js"></script>
+
+
+
 @endsection
