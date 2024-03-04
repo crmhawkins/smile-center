9ae5ef0baa649c1e2fce41377398052c392dfa8d
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-03-04 17:52:01 +0100

    Test

--- app/Http/Livewire/Disponible/IndexComponent.php
+++ app/Http/Livewire/Disponible/IndexComponent.php
@@ -31,11 +31,14 @@ class IndexComponent extends Component
         $fechaEvento = $date;
 
         $sumaTotalStockUsado = DB::table('presupuestos')
-        ->join('servicio_presupuesto', 'presupuestos.id', '=', 'servicio_presupuesto.presupuesto_id')
-        ->whereRaw('? BETWEEN presupuestos.diaEvento AND presupuestos.diaFinal', [$fechaEvento])
-        ->where('servicio_presupuesto.servicio_id', $servicioId)
-        ->selectRaw('SUM(CASE WHEN servicio_presupuesto.num_art_indef > 0 THEN servicio_presupuesto.num_art_indef ELSE COALESCE(NULLIF(servicio_articulo.stock_usado, 0), 1) END) AS total_stock_usado')
-        ->value('total_stock_usado');
+            ->join('servicio_presupuesto', 'presupuestos.id', '=', 'servicio_presupuesto.presupuesto_id')
+            ->whereRaw('? BETWEEN presupuestos.diaEvento AND presupuestos.diaFinal', [$fechaEvento])
+            ->where('servicio_presupuesto.servicio_id', $servicioId)
+            ->selectRaw('SUM(CASE
+                WHEN servicio_presupuesto.articulo_seleccionado != 0 THEN 1
+                ELSE servicio_presupuesto.num_art_indef
+                END) AS total_stock_usado')
+            ->value('total_stock_usado');
         dd($sumaTotalStockUsado);
             // Obtener el stock total fijo del artículo
             $stockTotal = DB::table('servicio_articulo')
