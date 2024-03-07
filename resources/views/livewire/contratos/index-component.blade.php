f4c0e14b01965d0d1e6e69d5c340fb9cdc8cc00d
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-03-07 15:28:39 +0100

    correccion contratos

--- resources/views/livewire/contratos/index-component.blade.php
+++ resources/views/livewire/contratos/index-component.blade.php
@@ -65,8 +65,6 @@
                                         </td>
                                         <td>{{ $contrato->authImagen > 0 ? 'Si' : 'No' }}</td>
                                         <td>{{ $contrato->authMenores > 0 ? 'Si' : 'No' }}</td>
-
-
                                         <td> <a href="contratos-edit/{{ $contrato->id }}"
                                                 class="btn btn-primary">Ver/Editar</a> </td>
                                     </tr>
@@ -89,57 +87,21 @@
 
 
 @section('scripts')
-    <script src="https://cdn.datatables.net/responsive/2.4.0/js/dataTables.responsive.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.3.4/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.3.4/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.3.4/js/buttons.print.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            console.log('entro');
-            $('#tableEmpresas').DataTable({
-                responsive: true,
-                dom: 'Bfrtip',
-                buttons: [
-                    'copy', 'csv', 'excel', 'pdf', 'print'
-                ],
-                buttons: [{
-                    extend: 'collection',
-                    text: 'Export',
-                    buttons: [{
-                            extend: 'pdf',
-                            className: 'btn-export'
-                        },
-                        {
-                            extend: 'excel',
-                            className: 'btn-export'
-                        }
-                    ],
-                    className: 'btn btn-info text-white'
-                }],
-                "language": {
-                    "lengthMenu": "Mostrando _MENU_ registros por página",
-                    "zeroRecords": "Nothing found - sorry",
-                    "info": "Mostrando página _PAGE_ of _PAGES_",
-                    "infoEmpty": "No hay registros disponibles",
-                    "infoFiltered": "(filtrado de _MAX_ total registros)",
-                    "search": "Buscar:",
-                    "paginate": {
-                        "first": "Primero",
-                        "last": "Ultimo",
-                        "next": "Siguiente",
-                        "previous": "Anterior"
-                    },
-                    "zeroRecords": "No se encontraron registros coincidentes",
-                }
-            });
+<script src="../assets/js/jquery.slimscroll.js"></script>
 
-            addEventListener("resize", (event) => {
-                location.reload();
-            })
-        });
-    </script>
+<script src="../plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../plugins/datatables/dataTables.bootstrap4.min.js"></script>
+<!-- Buttons examples -->
+<script src="../plugins/datatables/dataTables.buttons.min.js"></script>
+<script src="../plugins/datatables/buttons.bootstrap4.min.js"></script>
+<script src="../plugins/datatables/jszip.min.js"></script>
+<script src="../plugins/datatables/pdfmake.min.js"></script>
+<script src="../plugins/datatables/vfs_fonts.js"></script>
+<script src="../plugins/datatables/buttons.html5.min.js"></script>
+<script src="../plugins/datatables/buttons.print.min.js"></script>
+<script src="../plugins/datatables/buttons.colVis.min.js"></script>
+<!-- Responsive examples -->
+<script src="../plugins/datatables/dataTables.responsive.min.js"></script>
+<script src="../plugins/datatables/responsive.bootstrap4.min.js"></script>
+<script src="../assets/pages/datatables.init.js"></script>
 @endsection
