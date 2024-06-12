f80827a9e67b5705e09946cc5037954e862efd6a
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-06-12 17:03:58 +0200

    correcion

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -22,6 +22,7 @@
                     <a href="javascript:void(0);" class="waves-effect"><i class="fas fa-hand-holding-usd"></i><span> Presupuestos <span class="float-right menu-arrow"><i class="mdi mdi-chevron-right"></i></span> </span></a>
                     <ul class="submenu">
                         <li><a href="/admin/presupuestos">Ver Todos</a></li>
+                        <li><a href="/admin/presupuestos-flujo">Por Estados</a></li>
                         <li><a href="/admin/presupuestos-create">Crear Presupuesto</a></li>
                     </ul>
                 </li>
